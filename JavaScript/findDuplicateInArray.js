d876746b9aafbe28e5a1c31b2ea0f946b1ec0cd8
Hieu <tranhieu14310@gmail.com>
2021-01-24 21:07:05 +0700

    update at findDuplicateInArray.js

--- JavaScript/findDuplicateInArray.js
+++ JavaScript/findDuplicateInArray.js
@@ -12,6 +12,8 @@ findDuplicateInArray = function(arra1) {
             result.push(prop);
         }
     }
-
+    console.log(typeof result[0]);
     return result;
 };
+findDuplicateInArray([1,1,3,4,5,6,7,8,9,8,9]);
+// Trả về 1 array chứa các giá số trùng [1,8,9]
